936bd3ed80edb7838e9d6f609869cb98ba3ddbdf
Otniel Marques <otnielmarques3@gmail.com>
2024-09-10 20:53:06 -0300

    fundamentos 005

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const start = () =>{
-    let count = 0
-    while(count<=10){
-        console.log(count)
-        count++
+    while(true){
+        let opcao = "cadastrar"
+        switch(opcao){
+            case "cadastrar":
+                console.log("Vamos cadastrar")
+                break
+            case "listar":
+                console.log("Vamos listar")
+                break
+            case "sair":
+                return
+        }
     }
 }
 
