b5a4592868bf1b817928ea211652578f4664a790
Otniel Marques <otnielmarques3@gmail.com>
2024-09-13 02:11:42 -0300

    salvar metas

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const { select, input, checkbox } = require('@inquirer/prompts')
+const fs = require("fs").promises
 
 let mensagem = "Bem vindo ao app de metas!"
 
-let meta ={
-    value:'Tomar 3 litros de água por dia',
-    checked: false,
+let metas 
+
+const carregarMetas = async () =>{
+    try{
+        const dados = await fs.readFile("metas.json", "utf-8")
+        metas= JSON.parse(dados)
+    }
+    catch(erro){
+        metas = []
+    }
 }
-let metas = [ meta ]
+
+const salvarMetas = async () =>{
+    await fs.writeFile("metas.json", JSON.stringify(metas, null, 2))
+}
 
 const cadastrarMeta = async () =>{
     const meta = await input({message: "Digite a meta: "})
@@ -122,8 +133,11 @@ const mostrarMensagem = () =>{
 }
 
 const start = async () =>{
+    await carregarMetas()
+
     while(true){
         mostrarMensagem()
+        await salvarMetas()
         const opcao = await select({
             message: "Menu >",
             choices: [
@@ -178,4 +192,3 @@ const start = async () =>{
 }
 
 start();
-
